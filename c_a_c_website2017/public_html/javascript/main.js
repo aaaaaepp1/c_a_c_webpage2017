1a9d1ec9faee5ccc40a143d1178ea0cc25bb0aa3
aaaaaepp1 <epp1.ae.cap@gmail.com>
2017-05-28 15:43:07 +0900

    almost done creating index.html

---
+++ c_a_c_website2017/public_html/javascript/main.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function hoverMainMenu(idName, hover) {
+    //define id name
+    var imgIdName = 'mainMenu__' + idName + 'Link';
+    var moreButtonIdName = 'mainMenu__' + idName + 'LinkMoreButton';
+    
+    //get elment instance from id name
+    var imgElement = document.getElementById(imgIdName);
+    var moreButtonElement = document.getElementById(moreButtonIdName);
+    
+    //stop animation
+    $(imgElement).stop();
+    $(moreButtonElement).stop();
+    
+    //define animation of background img
+    var moveHeight = hover ? "430px" : "400px";
+    $(imgElement).animate({
+        height: moveHeight
+    }, 700, 'easeOutQuint');
+    
+    //define animation of more button
+    var moreButtonColor = hover ? "rgba(255, 255, 255,1.0)" : "rgba(255, 255, 255,0.0)";
+    var moreButtonTextColor = hover ? moreButtonElement.parentNode.style.backgroundColor : "#ffffff";
+    $(moreButtonElement).animate({
+        backgroundColor: moreButtonColor,
+        color: moreButtonTextColor
+    }, 700, 'easeOutQuint');
+    console.log(moreButtonColor);
+}
